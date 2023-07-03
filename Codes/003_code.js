dfe6d7be0db53d4f6fb8bb7397fdeafb3a0ce298
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-07-03 16:05:09 +0530

    js

---
+++ Codes/003_code.js
@@ -0,0 +1,14 @@
+var a=10;
+var b=5;
+
+var sum=a+b;
+var diff=a-b;
+var prod=a*b;
+var div=a/b;
+var mod=a%b;
+
+console.log(sum);
+console.log(diff);
+console.log(prod);
+console.log(div);
+console.log(mod);
