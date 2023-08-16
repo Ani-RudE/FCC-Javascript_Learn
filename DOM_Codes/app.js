f01ff6ccddd2489ea8e469d09f75808becd0d112
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-08-16 18:37:40 +0530

    main

--- DOM_Codes/app.js
+++ DOM_Codes/app.js
@@ -1,1 +1,23 @@
 console.log("M");
+
+
+// Event Listener
+// element.addEventListener("click", funtion);
+
+const buttonTwo=document.querySelector('.btn-2');
+function alertBtn()
+{
+     alert('JavaScript BTN-2');
+};
+
+buttonTwo.addEventListener("click", alertBtn);
+
+
+// Mouseover
+const newBackgroundColor=document.querySelector('.box-3');
+function changeBgColor()
+{
+     newBackgroundColor.style.backgroundColor='blue';
+}
+
+newBackgroundColor.addEventListener("mouseover", changeBgColor);
